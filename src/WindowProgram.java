c539cf24e50858b9bfed4f61d95a6fe4ad411aa5
Vestin1023 <vestin1023@gmail.com~>
2024-10-11 00:52:38 +0200

    Ui ser ut att funka

--- src/WindowProgram.java
+++ src/WindowProgram.java
@@ -1,11 +1,15 @@
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
 
 import se.miun.distsys.GroupCommunication;
 import se.miun.distsys.listeners.ChatMessageListener;
 import se.miun.distsys.messages.ChatMessage;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JTextPane;
 import java.awt.GridLayout;
@@ -24,6 +28,9 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 	JTextPane txtpnMessage = new JTextPane();
 	
 	GroupCommunication gc = null;
+	DefaultListModel<String> userListModel = new DefaultListModel<>();
+    JList<String> userList = new JList<>(userListModel);
+	
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -48,24 +55,33 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 
 	private void initializeFrame() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 600, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
+		frame.getContentPane().setLayout(new BorderLayout());
 		
-		JScrollPane scrollPane = new JScrollPane();
-		frame.getContentPane().add(scrollPane);
-		scrollPane.setViewportView(txtpnChat);
-		txtpnChat.setEditable(false);	
-		txtpnChat.setText("--== Group Chat ==--");
+		//left side
+		JScrollPane chatScrollPane = new JScrollPane(txtpnChat);
+        txtpnChat.setEditable(false);
+        txtpnChat.setText("--== Group Chat ==--");
+        frame.getContentPane().add(chatScrollPane, BorderLayout.CENTER);
+
+		//right side
+		JScrollPane userScrollPane = new JScrollPane(userList);
+        userScrollPane.setPreferredSize(new java.awt.Dimension(150, 0));
+        frame.getContentPane().add(userScrollPane, BorderLayout.EAST);
+
+		//down side
 		
-		txtpnMessage.setText("Message");
-		frame.getContentPane().add(txtpnMessage);
-		
-		JButton btnSendChatMessage = new JButton("Send Chat Message");
-		btnSendChatMessage.addActionListener(this);
-		btnSendChatMessage.setActionCommand("send");
-		
-		frame.getContentPane().add(btnSendChatMessage);
+		JPanel inputPanel = new JPanel(new BorderLayout());
+        txtpnMessage.setText("Message");
+        inputPanel.add(txtpnMessage, BorderLayout.CENTER);
+
+        JButton btnSendChatMessage = new JButton("Send");
+        btnSendChatMessage.addActionListener(this);
+        btnSendChatMessage.setActionCommand("send");
+        inputPanel.add(btnSendChatMessage, BorderLayout.EAST);
+        
+        frame.getContentPane().add(inputPanel, BorderLayout.SOUTH);
 		
 		frame.addWindowListener(new java.awt.event.WindowAdapter() {
 	        public void windowClosing(WindowEvent winEvt) {
@@ -83,6 +99,7 @@ public class WindowProgram implements ChatMessageListener, ActionListener {
 	
 	@Override
 	public void onIncomingChatMessage(ChatMessage chatMessage) {	
-		txtpnChat.setText(chatMessage.chat + "\n" + txtpnChat.getText());				
+		txtpnChat.setText(chatMessage.chat + "\n" + txtpnChat.getText());
+		userListModel.addElement("New User");				
 	}
 }
